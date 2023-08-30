69f563a3415aa6dc58ba1a90a3203d732e9107a9
Ngori Januário <ngorijanuario@gmail.com>
2023-08-30 05:20:55 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ngori Januario on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
