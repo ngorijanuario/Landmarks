19efa858a2965ab766227a24722c139ff913b14e
Ngori Januário <ngorijanuario@gmail.com>
2023-08-31 09:39:06 +0100

    Create the List of Landmarks

---
+++ Landmarks/Views /LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ngori Januario on 31/08/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
