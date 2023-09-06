af74fc171d3e1b68b6cc1c24bf5217d30190fb0a
Ngori Januário <ngorijanuario@gmail.com>
2023-09-06 17:32:55 +0100

    Add a Category View

---
+++ Landmarks/Views /CategoryHome.swift
@@ -0,0 +1,18 @@
+//  Created by Ngori Januario on 06/09/23.
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
