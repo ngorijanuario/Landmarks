da34fa39b8af10df9130aa5c41dec78c59b9fc81
Ngori Januário <ngorijanuario@gmail.com>
2023-09-06 18:09:43 +0100

    Add Navigation Between Sections

--- Landmarks/Views /ContentView.swift
+++ Landmarks/Views /ContentView.swift
@@ -1,11 +1,33 @@
 //  Created by Ngori Januario on 30/08/23.
 import SwiftUI
-
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+
+    enum Tab {
+        case featured
+        case list
+    }
+
+
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
+
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
